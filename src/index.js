965a2189910d5aad64e86eb13a0312983cd60811
mefa <mefaba@outlook.com>
2021-12-28 21:46:44 -0500

    version 0.5.1

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-import React, { useRef, useEffect, useState } from 'react'
-import styles from './styles.module.css'
+import React, { useRef, useEffect, useState } from "react";
 
 export const ReactScrollSlide = (props) => {
   const [slideIndex, setSlideIndex] = useState({
-    current: 0
-  })
-  const slideEl = useRef({})
+    current: 0,
+  });
+  const slideEl = useRef({});
 
   //inject ref to children as properties to find top height of children
   //find top height of children via element.getBoundingClientRect().top
@@ -13,65 +12,57 @@ export const ReactScrollSlide = (props) => {
   //if infiniteScroll false do not loop index of slides. Use mode % to create infite index loops
   //for better: scrollIntoView is so fast, create a customazable scroll animation. Idea change viewport offset height by css and animate with css.
   useEffect(() => {
-    document.addEventListener('wheel', handleScroll, {
-      passive: false
-    })
+    document.addEventListener("wheel", handleScroll, {
+      passive: false,
+    });
 
     return () => {
-      document.removeEventListener('wheel', handleScroll, {
-        passive: false
-      })
-    }
-  }, [])
+      document.removeEventListener("wheel", handleScroll, {
+        passive: false,
+      });
+    };
+  }, []);
 
   const indexRangeLimiter = (xNumber, min, max) => {
     if (xNumber > max) {
-      return max
+      return max;
     } else if (xNumber < min) {
-      return min
+      return min;
     } else {
-      return xNumber
+      return xNumber;
     }
-  }
+  };
   const handleScroll = (e) => {
-    e.preventDefault()
-    const slideLength = Object.keys(slideEl.current).length - 1
+    e.preventDefault();
+    const slideLength = Object.keys(slideEl.current).length - 1;
 
     if (e.deltaY > 0) {
       setSlideIndex((prevSlideIndex) => {
-        let newIndex = indexRangeLimiter(
-          prevSlideIndex.current + 1,
-          0,
-          slideLength
-        )
-        scrollToSlide(newIndex)
-        return { ...prevSlideIndex, current: newIndex }
-      })
+        let newIndex = indexRangeLimiter(prevSlideIndex.current + 1, 0, slideLength);
+        scrollToSlide(newIndex);
+        return { ...prevSlideIndex, current: newIndex };
+      });
     } else if (e.deltaY < 0) {
       setSlideIndex((prevSlideIndex) => {
-        let newIndex = indexRangeLimiter(
-          prevSlideIndex.current - 1,
-          0,
-          slideLength
-        )
-        scrollToSlide(newIndex)
-        return { ...prevSlideIndex, current: newIndex }
-      })
+        let newIndex = indexRangeLimiter(prevSlideIndex.current - 1, 0, slideLength);
+        scrollToSlide(newIndex);
+        return { ...prevSlideIndex, current: newIndex };
+      });
     }
-  }
+  };
 
   const scrollToSlide = (index) => {
-    slideEl.current[index].scrollIntoView()
-  }
+    slideEl.current[index].scrollIntoView();
+  };
 
   return (
     <React.Fragment>
       {props.children.map((child, index) =>
         React.cloneElement(child, {
           key: index,
-          ref: (ref) => (slideEl.current[index] = ref)
+          ref: (ref) => (slideEl.current[index] = ref),
         })
       )}
     </React.Fragment>
-  )
-}
+  );
+};
