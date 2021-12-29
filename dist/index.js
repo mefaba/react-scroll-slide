cecdfcaeb15ccb31b6603d6e73a94c8e2a8918e8
mefa <mefaba@outlook.com>
2021-12-28 23:22:15 -0500

    update re-run bundler

--- dist/index.js
+++ dist/index.js
@@ -1,25 +1,26 @@
-var e = require("react");
+var e = require("react"),
+  r = require("react/jsx-runtime");
 function t(e) {
   return e && "object" == typeof e && "default" in e ? e : { default: e };
 }
-var r = /*#__PURE__*/ t(e);
-function n() {
+var n = /*#__PURE__*/ t(e);
+function u() {
   return (
-    (n =
+    (u =
       Object.assign ||
       function (e) {
-        for (var t = 1; t < arguments.length; t++) {
-          var r = arguments[t];
-          for (var n in r) Object.prototype.hasOwnProperty.call(r, n) && (e[n] = r[n]);
+        for (var r = 1; r < arguments.length; r++) {
+          var t = arguments[r];
+          for (var n in t) Object.prototype.hasOwnProperty.call(t, n) && (e[n] = t[n]);
         }
         return e;
       }),
-    n.apply(this, arguments)
+    u.apply(this, arguments)
   );
 }
 exports.ReactScrollSlide = function (t) {
-  var u = e.useState({ current: 0 })[1],
-    c = e.useRef({});
+  var c = e.useState({ current: 0 })[1],
+    a = e.useRef({});
   e.useEffect(function () {
     return (
       document.addEventListener("wheel", o, { passive: !1 }),
@@ -28,37 +29,35 @@ exports.ReactScrollSlide = function (t) {
       }
     );
   }, []);
-  var a = function (e, t, r) {
-      return e > r ? r : e < t ? t : e;
+  var i = function (e, r, t) {
+      return e > t ? t : e < r ? r : e;
     },
     o = function (e) {
       e.preventDefault();
-      var t = Object.keys(c.current).length - 1;
+      var r = Object.keys(a.current).length - 1;
       e.deltaY > 0
-        ? u(function (e) {
-            var r = a(e.current + 1, 0, t);
-            return f(r), n({}, e, { current: r });
+        ? c(function (e) {
+            var t = i(e.current + 1, 0, r);
+            return f(t), u({}, e, { current: t });
           })
         : e.deltaY < 0 &&
-          u(function (e) {
-            var r = a(e.current - 1, 0, t);
-            return f(r), n({}, e, { current: r });
+          c(function (e) {
+            var t = i(e.current - 1, 0, r);
+            return f(t), u({}, e, { current: t });
           });
     },
     f = function (e) {
-      c.current[e].scrollIntoView();
+      a.current[e].scrollIntoView();
     };
-  return h(
-    r.default.Fragment,
-    null,
-    t.children.map(function (e, t) {
-      return r.default.cloneElement(e, {
-        key: t,
+  /*#__PURE__*/ return r.jsx(n.default.Fragment, {
+    children: t.children.map(function (e, r) {
+      /*#__PURE__*/ return n.default.cloneElement(e, {
+        key: r,
         ref: function (e) {
-          return (c.current[t] = e);
+          return (a.current[r] = e);
         },
       });
-    })
-  );
+    }),
+  });
 };
 //# sourceMappingURL=index.js.map
